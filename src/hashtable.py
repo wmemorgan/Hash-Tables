ac79a63c84866ad494a6c9f24e078b01a0d9a25a
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-01-30 19:36:51 +0800

    Add hash, retrieve, resize, and remove methods
    Add first pass insert method without collision handling

--- src/hashtable.py
+++ src/hashtable.py
@@ -23,7 +23,14 @@ class HashTable:
 
         You may replace the Python hash with DJB2 as a stretch goal.
         '''
-        return hash(key)
+        output_index = 0
+        WEIRD_PRIME = 31
+        for indx in range(min(len(key), 100)):
+            value = ord(key[indx]) - 96
+            output_index = (output_index + WEIRD_PRIME + value) % len(key)
+
+        print(f"output_index: {output_index}")
+        return output_index
 
 
     def _hash_djb2(self, key):
@@ -40,7 +47,9 @@ class HashTable:
         Take an arbitrary key and return a valid integer index
         within the storage capacity of the hash table.
         '''
-        return self._hash(key) % self.capacity
+        index = self._hash(key) % self.capacity
+        print(f"_hash_mod index: {index}")
+        return index
 
 
     def insert(self, key, value):
@@ -51,7 +60,13 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        if None not in self.storage:
+            self.resize
+            self.storage[self._hash_mod(key)] = (key, value)
+        else:
+            self.storage[self._hash_mod(key)] = (key, value)
+            
+        
 
 
 
@@ -63,7 +78,12 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        print(
+            f"self.storage[self._hash_mod(key)]: {self.storage[self._hash_mod(key)]}")
+        if self.storage[self._hash_mod(key)] is not None:
+            self.storage[self._hash_mod(key)] = None
+        else:
+            print("key does not exists")
 
 
     def retrieve(self, key):
@@ -74,7 +94,13 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        # print(f"hash_mod: {self._hash_mod(key)}")
+        kv = self.storage[self._hash_mod(key)]
+        # print(f"kv: {self.storage[self._hash_mod(key)]}")
+        if kv is not None:
+            return kv[1]
+        else:
+            return None
 
 
     def resize(self):
@@ -84,7 +110,14 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        self.capacity = self.capacity * 2
+        temp_storage = [None] * self.capacity
+
+        for i in range(self.capacity // 2):
+            temp_storage[i] = self.storage[i]
+        
+        print(f"temp_storage: {temp_storage}")
+        self.storage = temp_storage
 
 
 
@@ -93,14 +126,15 @@ if __name__ == "__main__":
 
     ht.insert("line_1", "Tiny hash table")
     ht.insert("line_2", "Filled beyond capacity")
-    ht.insert("line_3", "Linked list saves the day!")
+    # ht.insert("line_3", "Linked list saves the day!")
 
     print("")
 
     # Test storing beyond capacity
     print(ht.retrieve("line_1"))
     print(ht.retrieve("line_2"))
-    print(ht.retrieve("line_3"))
+    # print(ht.retrieve("line_3"))
+    print(f"storage: {ht.storage}")
 
     # Test resizing
     old_capacity = len(ht.storage)
@@ -110,8 +144,13 @@ if __name__ == "__main__":
     print(f"\nResized from {old_capacity} to {new_capacity}.\n")
 
     # Test if data intact after resizing
-    print(ht.retrieve("line_1"))
-    print(ht.retrieve("line_2"))
+    print(f"line_1: {ht.retrieve('line_1')}")
+    print(f"line_2: {ht.retrieve('line_2')}")
+    # print(ht.retrieve("line_2"))
+    # print(ht.retrieve("line_3"))
+    ht.remove('line_1')
+    ht.insert("line_3", "Linked list saves the day!")
     print(ht.retrieve("line_3"))
+    print(f"storage: {ht.storage}")
 
     print("")
